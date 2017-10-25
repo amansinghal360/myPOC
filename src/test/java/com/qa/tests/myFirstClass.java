bc6b336e7c5bddc92b29da8f4272a4284a1a5170
amansinghal360 <aman.singhal@tothenew.com>
2017-10-25 13:29:49 +0530

    first commit

---
+++ src/test/java/com/qa/tests/myFirstClass.java
@@ -0,0 +1,32 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class myFirstClass {
+
+	@Test
+	public void add() {
+		System.out.println("add");
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void sub() {
+		System.out.println("sub");
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void mul() {
+		System.out.println("mul");
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void div() {
+		System.out.println("div");
+		Assert.assertTrue(true);
+	}
+
+}
