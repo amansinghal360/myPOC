371da6963cc7180c0d8b75dc396920c4618564d9
amansinghal360 <aman.singhal@tothenew.com>
2017-10-25 13:49:41 +0530

    second commit

--- src/test/java/com/qa/tests/myFirstClass.java
+++ src/test/java/com/qa/tests/myFirstClass.java
@@ -7,25 +7,21 @@ public class myFirstClass {
 
 	@Test
 	public void add() {
-		System.out.println("add");
 		Assert.assertTrue(true);
 	}
 
 	@Test
 	public void sub() {
-		System.out.println("sub");
 		Assert.assertTrue(true);
 	}
 
 	@Test
 	public void mul() {
-		System.out.println("mul");
 		Assert.assertTrue(true);
 	}
 
 	@Test
 	public void div() {
-		System.out.println("div");
 		Assert.assertTrue(true);
 	}
 
